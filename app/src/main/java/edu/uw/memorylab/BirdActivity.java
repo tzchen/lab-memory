1ad6bffb9b1fc50d9554d521a1f1597d6fc520fd
tiffany <tzchen@uw.edu>
2017-05-23 14:53:44 -0700

    finished lab

--- app/src/main/java/edu/uw/memorylab/BirdActivity.java
+++ app/src/main/java/edu/uw/memorylab/BirdActivity.java
@@ -30,4 +30,18 @@ public class BirdActivity extends AppCompatActivity {
         birdView.setImageDrawable(image);
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        ImageView birdView = (ImageView) findViewById(R.id.imgBird);
+        birdView.setImageDrawable(null);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Drawable image = ContextCompat.getDrawable(this, R.drawable.hummingbird);
+        ImageView birdView = (ImageView)findViewById(R.id.imgBird);
+        birdView.setImageDrawable(image);
+    }
 }
